d8ed08640ff86eff68135461890d0b74cfcde5c6
simonajell <Simona.Jell@campus.lmu.de>
2023-04-05 11:36:47 +0200

    umformuliert

--- Erster_Versuch.R
+++ Erster_Versuch.R
@@ -13,6 +13,8 @@ library(HDInterval)
 library(ggdist)
 library(latex2exp)
 
+library(rlang)
+
 
 utils::download.file(url="https://data.mendeley.com/public-files/datasets/25yjwbphn4/files/05601aea-ad43-4a93-8d28-adf0fa74b3c9/file_downloaded",
                      destfile = "CYPtrialData.xlsx")
@@ -21,58 +23,66 @@ data<-read_xlsx("CYPtrialData.xlsx")
 data<-data[complete.cases(data),]
 data<-data[which(data$LOS>24*3),]
 
+# In Faktoren umwandeln
 data$Assignment<-factor(data$Assignment,levels=c("S","G"))
 data$'RACE/ETHNICITY'<-factor(data$'RACE/ETHNICITY',levels=c("W","B","L","O/U"))
 
-names(data)[which(names(data)=="RACE/ETHNICITY")]<-"Ethnicity"
+# Umbenennen
+names(data)[which(names(data)=="RACE/ETHNICITY")] <- "Ethnicity"
 
-## Section 4.1 selbst versuchen
+
 # Modell
-blogisticLOS<-brm(as.numeric(LOS>median(LOS))~Assignment+AGE+GENDER+Ethnicity,family=bernoulli(),data=data,seed=2022)
-# Werte für Variablen aus Modell gewonnen
+blogisticLOS <- brm(as.numeric(LOS>median(LOS)) ~ Assignment + AGE + GENDER + Ethnicity,
+                  family = bernoulli(), data = data, seed = 2022)
+
+# Werte für Schätzer (beta) schätzen: 1000-mal
 set.seed(2022)
-logLOS_draws<-blogisticLOS %>%
-  spread_draws(b_Intercept , b_AssignmentG ,
-               b_AGE, b_GENDERM , b_EthnicityB , b_EthnicityL ,
-               b_EthnicityODU ,
+logLOS_draws <- blogisticLOS %>%
+  spread_draws(b_Intercept, b_AssignmentG ,
+               b_AGE, b_GENDERM, b_EthnicityB, b_EthnicityL,
+               b_EthnicityODU,
                ndraws = 1000) %>%
-  dplyr::select(- .chain,-.iteration,-.draw) %>%
+  dplyr::select(- .chain, -.iteration, -.draw) %>%
   as.data.frame()
 
 # Datensatz mit dummy Spalten (binär) erstellen
-df<-dplyr::select(dummy_cols(data[,c("AGE","Assignment","GENDER","Ethnicity")],"Ethnicity"),-Ethnicity)
-names(df)<-c("age","assignmentG","genderM","ethW","ethB","ethL","ethO") #Spaltennamen ändern
-df$genderM<-ifelse(df$genderM=="M",1,0) #Geschlecht binär kodieren
-df$assignmentG<-ifelse(df$assignmentG=="G",1,0) #assignment binär kodieren
+df <- dplyr::select(dummy_cols(data[, c("AGE", "Assignment", "GENDER", "Ethnicity")], "Ethnicity"), -Ethnicity)
+names(df) <- c("age", "assignmentG", "genderM", "ethW", "ethB", "ethL", "ethO") #Spaltennamen ändern
+df$genderM <- ifelse(df$genderM=="M",1,0) #Geschlecht binär kodieren
+df$assignmentG <- ifelse(df$assignmentG=="G",1,0) #assignment binär kodieren
 
 ## A2'
 # arithmetisches Mittel über alle Beobachtungen wird berechnet, wenn man interessierenden Ethnizität auf 1 setzt
-ethCat1<-list()
-ethCat1$ethW<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(mutate(df,ethW=1,ethB=0,ethL=0,ethO=0),-"ethW")))/
-    nrow(df)})
-ethCat1$ethB<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(mutate(df,ethW=0,ethB=1,ethL=0,ethO=0),-"ethW")))/
-    nrow(df)})
-ethCat1$ethL<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(mutate(df,ethW=0,ethB=0,ethL=1,ethO=0),-"ethW")))/
-    nrow(df)})
-ethCat1$ethO<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(mutate(df,ethW=0,ethB=0,ethL=0,ethO=1),-"ethW")))/
-    nrow(df)})
+ethCat1<-list(ethW = 0, ethB = 0, ethL = 0, ethO = 0)
+eth_rule_W <- c(1, 0, 0, 0)
+eth_ruleB <- c(0, 1, 0, 0)
+eth_ruleL <- c(0, 0, 1, 0)
+eth_ruleO <- c(0, 0, 0, 1)
+
+for(i in seq_along(1:4)) {
+  ethCat1[[i]] <- apply(logLOS_draws, 1, function(x){sum(intval(x, dplyr::select(mutate(df,
+   ethW = eth_rule_W[i], ethB = eth_ruleB[i], ethL = eth_ruleL[i], ethO = eth_ruleO[i]),
+   -"ethW")))/nrow(df)})
+}
 
 ## A2"
 # arithmetisches Mittel über alle Beobachtungen, die gewünschte Ethnizität haben -> 1000-mal
-ethCat2<-list()
-ethCat2$ethW<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(df[which(df$ethW==1),],-"ethW")))/length(which(df$ethW==1))})
-ethCat2$ethB<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(df[which(df$ethB==1),],-"ethW")))/length(which(df$ethB==1))})
-ethCat2$ethL<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(df[which(df$ethL==1),],-"ethW")))/length(which(df$ethL==1))})
-ethCat2$ethO<-apply(logLOS_draws,1,function(x){sum(intval(x,dplyr::select(df[which(df$ethO==1),],-"ethW")))/length(which(df$ethO==1))})
+ethCat2<-list(ethW = 0, ethB = 0, ethL = 0, ethO = 0)
+eth_cat_num <- c(4, 5, 6, 7)
+for(i in seq_along(1:4)) {
+  ethCat2[[i]] <- apply(logLOS_draws, 1, function(x){
+    sum(intval(x, dplyr::select(df[which(df[i] == 1), ], -"ethW")))/
+      length(which(df[i] == 1))})
+}
 
-# Dataframe der für jede Ethnizität die eben berechneten Werte angibt und den Mittelwert der Ethnizität
+# Dataframe, der für jede Ethnizität die eben berechneten Werte angibt und den Mittelwert der Ethnizität
 Pcat1<-ldply(ethCat1, data.frame) %>% mutate(.id=as.factor(.id))
 names(Pcat1)<-c("Ethnicity","value") #Umbenennen der Spalten
 Pcat1<-merge(Pcat1,
              Pcat1 %>%
                group_by(Ethnicity)%>%
-               dplyr::summarize_at("value",mean) %>% dplyr::rename(mean=value),
-             by="Ethnicity") #Eine Spalte für den Mittelwert der ganzen Ethnizität hinzufügen
+               dplyr::summarize_at("value",mean) %>%
+               dplyr::rename(mean=value),by="Ethnicity") #Eine Spalte für den Mittelwert der ganzen Ethnizität hinzufügen
 levels(Pcat1$Ethnicity)<- list(White = "ethW", Latinx = "ethL", Black = "ethB", Other = "ethO") #Umbenennen der Ethnizitäten
 # Das selbe für ethCat2
 Pcat2<-ldply(ethCat2, data.frame) %>% mutate(.id=as.factor(.id))
